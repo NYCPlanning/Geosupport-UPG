d67bc94dbd9ef81dd55820369877480d90839742
Gloria Katz <gkatz@planning.nyc.gov>
2018-08-07 12:50:52 -0400

    V18.3 site deleted

--- site/pageFooter.js
+++
@@ -1,6 +0,0 @@
-exports.footer = {
-  height: "0.2cm",
-  contents: function(pageNum, numPages) {
-    return "<p><center><span style='float:none; font-size: 10px; color:gray;'>" + pageNum + " of " + numPages + "</span></center></p>"
-  }
-}
